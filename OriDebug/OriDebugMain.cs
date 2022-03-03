91b305a71abb9d4072e2d46fcc5e60b9cc21810c
SharpIce <vuprbl@163.com>
2022-03-04 00:12:17 +0800

    第一个版本发布，只兼容奥日与黑暗森林终极版

---
+++ OriDebug/OriDebugMain.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace OriDebug
+{
+    [BepInPlugin("com.sharpice.oridebug", "OriDebug", "1.0.0")]
+    [BepInProcess("oriDE.exe")]
+    public class OriDebugMain : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            CheatsHandler.DebugAlwaysEnabled = true;
+        }
+    }
+}
